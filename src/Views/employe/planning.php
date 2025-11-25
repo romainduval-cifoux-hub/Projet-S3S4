b68637b3af77f0120de068cc5b638e8ee661caa8
Hugo VITRY <vitry.hugo2006@gmail.com>
2025-11-25 16:05:38 +0100

    ajout du mois et etirement du planning (creation d'un css à part pour le planning de l'employe

--- src/Views/employe/planning.php
+++ src/Views/employe/planning.php
@@ -8,7 +8,7 @@
     <link href="<?= BASE_URL ?>/public/assets/shared/charte-graphique.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/header/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/header/position.css" rel="stylesheet">
-    <link href="<?= BASE_URL ?>/public/assets/chef/css/style.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>/public/assets/employe/css/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/aside/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/footer/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/footer/position.css" rel="stylesheet">
