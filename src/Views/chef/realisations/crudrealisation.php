f45b92623f818920b48351106e7a49c333e0ec9e
Armin Osmanovic <armin.osmanovic@etu.univ-lyon1.fr>
2025-12-10 13:55:11 +0100

    CSS du CRUD

--- src/Views/chef/realisations/crudrealisation.php
+++ src/Views/chef/realisations/crudrealisation.php
@@ -7,7 +7,7 @@
     <link href="<?= BASE_URL ?>/public/assets/shared/header/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/header/position.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/aside/style.css" rel="stylesheet">
-    <link href="<?= BASE_URL ?>/public/assets/chef/realisations/crudrealisation/style.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>/public/assets/chef/realisations/liste/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/footer/style.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/public/assets/shared/footer/position.css" rel="stylesheet">
 </head>
