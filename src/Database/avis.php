4d494d6d91518aa2655d4035c29ae63622168d6a
Romain DC <romain.duval--cifoux@etu.univ-lyon1.fr>
2026-01-18 17:22:08 +0100

    Modification de la fonction pour récupérer les avis pour ne prendre en compte que les clients qui ont un compte client (avant ça affichait aussi les avis des admins déjà en base et les user qui sont clients mais pas dans la table client)

--- src/Database/avis.php
+++ src/Database/avis.php
@@ -3,34 +3,33 @@
 function getAvisEtMoyenne(PDO $pdo): array
 {
     $sql = "
-    SELECT 
-        c.nom_client     AS nom,
-        c.prenom_client  AS prenom,
-        c.photo   AS photo,
-        a.date_commentaire AS date,
-        a.commentaire,
-        a.note
-    FROM avis a
-    INNER JOIN users u   ON a.user_id = u.id
-    INNER JOIN clients c ON c.id_client = u.id
-    ORDER BY a.date_commentaire DESC
-";
-
+        SELECT 
+            c.nom_client AS nom,
+            c.prenom_client AS prenom,
+            c.photo AS photo,
+            a.date_commentaire AS date,
+            a.commentaire,
+            a.note
+        FROM avis a
+        INNER JOIN users u   ON a.user_id = u.id
+        INNER JOIN clients c ON c.user_id = u.id
+        ORDER BY a.date_commentaire DESC
+    ";
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Calcul de la moyenne
+    // Calcul moyenne
     $moyenne = 0;
     if (count($avis) > 0) {
         $totalNotes = 0;
         foreach ($avis as $a) {
             $totalNotes += (int)$a['note'];
         }
-        $moyenne = round($totalNotes / count($avis)); // entier entre 1 et 5
+        $moyenne = round($totalNotes / count($avis));
     }
 
-    // On renvoie les deux infos
     return [$avis, $moyenne];
 }
+
